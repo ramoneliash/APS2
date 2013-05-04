1649f5eae9dd910a1472373027a4d03143ef0cb6
ramoneliash <ramoneliash@hotmail.com>
2013-05-03 20:29:34 -0600

    Mi primer test de constructor

---
+++ SOLUTIONFRACTIONS/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WindowsFormsApplication1;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestConstructorDefault()
+        {
+            FRACIONES F = new FRACIONES();
+            int numerador = F.Numerador;
+            int denominador = F.Denominador;
+            bool b = (numerador == 1) && (denominador == 1);
+            Assert.AreEqual(true, b);
+
+        }
+    }
+}
--- SOLUTIONFRACTIONS/WindowsFormsApplication1/FRACIONES.cs
+++ SOLUTIONFRACTIONS/WindowsFormsApplication1/FRACIONES.cs
@@ -8,5 +8,17 @@ namespace WindowsFormsApplication1
 {
     public class FRACIONES
     {
+
+        public int Numerador { get; set; }
+
+        public int Denominador { get; set; }
+
+        public FRACIONES()
+             {
+
+            this.Numerador = 1;
+            this.Denominador = 1;
+            }
+    
     }
 }
